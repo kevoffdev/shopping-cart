e4e0a441f26ce3fcce40edffb67f07394d040a3c
kevoffdev <kevinsuviacruz@gmail.com>
2024-11-22 19:16:59 -0300

    add shadcn components

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -2,8 +2,17 @@ import { Product } from "../types";
 import { useFilter } from "../hooks/useFilter";
 import { useCart } from "../hooks/useCart";
 
-import { Filters } from "./Filters";
 import { AddToCartIcon, RemoveFromCartIcon } from "./Icon";
+import { Button } from "./ui/button";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export const Products = () => {
   const { filterProducts } = useFilter();
@@ -14,45 +23,50 @@ export const Products = () => {
   };
 
   return (
-    <main className="my-4">
-      <Filters />
-      <section className="grid grid-cols-4 gap-4">
-        {filterProducts().map((product) => {
-          const isProductInCart = checkProductInCart(product);
+    <section className="grid  sm:grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4">
+      {filterProducts().map((product) => {
+        const isProductInCart = checkProductInCart(product);
 
-          return (
-            <article
-              key={product.id}
-              className="flex flex-col justify-between overflow-hidden bg-zinc-950 rounded-xl"
-            >
-              <img
-                alt={product.title}
-                className="block object-cover w-full bg-gray-700"
-                src={product.thumbnail}
-              />
-              <div className="px-2 mt-2 text-center">
-                <strong className="opacity-90">
-                  {product.title} - ${product.price}
-                </strong>
+        return (
+          <Card
+            key={product.id}
+            className="grid grid-rows-[90px,auto,auto] bg-card place-items-center"
+          >
+            <CardHeader>
+              <CardTitle className="text-lg">{product.title}</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div>
+                <img alt={product.title} className="object-cover" src={product.thumbnail} />
+              </div>
+              <CardTitle className="flex flex-col gap-2 text-base">
+                <span>{product.title}</span>
+                <span>${product.price}</span>
+              </CardTitle>
+              <CardDescription className="mt-2">
                 <p>
                   Quantity:{" "}
                   {quantity[product.id] > 0 ? product.stock - quantity[product.id] : product.stock}
                 </p>
-              </div>
-              <div className="text-center">
-                <button
-                  className={`p-3 my-2 bg-gray-700 rounded-xl ${isProductInCart ? "hover:text-red-400" : "hover:text-green-400"} transition`}
-                  onClick={
-                    isProductInCart ? () => removeFromCart(product.id) : () => addToCart(product)
-                  }
+              </CardDescription>
+            </CardContent>
+            <CardFooter>
+              {isProductInCart ? (
+                <Button
+                  className="px-8 hover:text-red-400"
+                  onClick={() => removeFromCart(product.id)}
                 >
-                  {isProductInCart ? <RemoveFromCartIcon /> : <AddToCartIcon />}
-                </button>
-              </div>
-            </article>
-          );
-        })}
-      </section>
-    </main>
+                  <RemoveFromCartIcon />
+                </Button>
+              ) : (
+                <Button className="px-8 hover:text-green-400" onClick={() => addToCart(product)}>
+                  <AddToCartIcon />
+                </Button>
+              )}
+            </CardFooter>
+          </Card>
+        );
+      })}
+    </section>
   );
 };
