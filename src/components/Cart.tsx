08d4796da70c28de1eea1f84ffc088cd4616e11a
kevoffdev <kevinsuviacruz@gmail.com>
2024-11-22 19:13:40 -0300

    replacing ui with sadcn components

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,72 +1,98 @@
-import { useId } from "react";
-
 import { useCart } from "../hooks/useCart";
 
 import { CartIcon, Trash } from "./Icon";
+import { Button } from "./ui/button";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 export const Cart = () => {
   const { cart, quantity, removeFromCart, incrementProductCart, decrementProductCart } = useCart();
-  const cartCheckBoxId = useId();
+
+  const totalProducts = Object.keys(quantity);
+
+  const sumTotalProducts = cart
+    .reduce(
+      (sum, product) =>
+        quantity[product.id] ? (sum += product.price * quantity[product.id]) : sum,
+      0,
+    )
+    .toFixed(2);
 
   return (
-    <div className="relative flex justify-end">
-      <label
-        className="block h-full p-3 bg-blue-400 rounded-full cursor-pointer hover:bg-blue-300"
-        htmlFor={cartCheckBoxId}
-      >
-        <CartIcon />
-      </label>
-      <input className="hidden" id={cartCheckBoxId} type="checkbox" />
-
-      <aside className="absolute right-0 z-10 text-black bg-white border border-gray-200 rounded-lg shadow-lg top-14 w-[480px] opacity-0 pointer-events-none transition-opacity duration-300 ease-in-out transform scale-95">
-        <label
-          className="absolute text-gray-600 cursor-pointer right-2 top-2 hover:text-gray-800"
-          htmlFor={cartCheckBoxId}
-        >
-          X
-        </label>
-
-        <ul className="p-4">
-          <h2 className="mb-2 text-lg font-semibold">Cart</h2>
-          {cart.length > 0 ? (
-            cart.map((item) => {
-              return (
-                <li key={item.id} className="flex items-center mb-4">
-                  <img
-                    alt={item.title}
-                    className="object-contain w-12 h-10 border-2 rounded-lg"
-                    src={item.thumbnail}
-                  />
-                  <div className="flex-1 ml-4">
-                    <strong>{item.title}</strong> - ${item.price}
-                  </div>
-                  <div className="flex items-center gap-2">
-                    <button
-                      className="p-1 border"
-                      onClick={() => incrementProductCart(item.id, item.stock)}
-                    >
-                      +
-                    </button>
-                    <small className="">{quantity[item.id]}</small>
-                    <button className="p-1 border" onClick={() => decrementProductCart(item.id)}>
-                      -
-                    </button>
-                    <button onClick={() => removeFromCart(item.id)}>{<Trash />}</button>
-                  </div>
-                </li>
-              );
-            })
-          ) : (
-            <p className="text-lg text-center">Cart empy.</p>
-          )}
-        </ul>
-      </aside>
-      <style>{`
-        input:checked + aside {
-          opacity: 1;
-          pointer-events: auto;
-        }
-      `}</style>
-    </div>
+    <Sheet>
+      <div className="flex items-center justify-end">
+        <SheetTrigger className="w-auto p-3 rounded-full text-primary-foreground bg-primary">
+          <CartIcon />
+        </SheetTrigger>
+      </div>
+      <SheetContent className="w-[250px] md:w-full overflow-auto bg-background text-primary">
+        <SheetHeader>
+          <SheetTitle>Cart</SheetTitle>
+        </SheetHeader>
+        <SheetDescription className="flex flex-col gap-2">
+          <span>Products in the cart: {totalProducts.length}</span>
+          <span>Total price ${sumTotalProducts}</span>
+        </SheetDescription>
+        <div className="text-primary">
+          <ul className="p-4">
+            {cart.length > 0 ? (
+              cart.map((item) => {
+                return (
+                  <li
+                    key={item.id}
+                    className="flex flex-col items-center justify-between gap-2 py-5 border-b md:flex-row border-foreground"
+                  >
+                    <div className="flex flex-col items-center justify-center gap-2 md:flex-row">
+                      <img
+                        alt={item.title}
+                        className="object-contain w-12 h-10 border rounded-lg border-foreground"
+                        src={item.thumbnail}
+                      />
+                    </div>
+                    <div className="flex flex-col items-center flex-1 gap-1 ml-2 md:items-start">
+                      <span className="font-bold">{item.title}</span>
+                      <span>${(item.price * quantity[item.id]).toFixed(2)}</span>
+                    </div>
+                    <div className="grid grid-rows-2 gap-2">
+                      <div className="grid grid-cols-3 place-items-center">
+                        <Button
+                          className="w-full md:w-auto"
+                          onClick={() => incrementProductCart(item.id, item.stock)}
+                        >
+                          +
+                        </Button>
+                        <small>{quantity[item.id]}</small>
+                        <Button
+                          className="w-full md:w-auto"
+                          onClick={() => decrementProductCart(item.id)}
+                        >
+                          -
+                        </Button>
+                      </div>
+                      <Button
+                        className="w-full md:w-auto"
+                        variant={"destructive"}
+                        onClick={() => removeFromCart(item.id)}
+                      >
+                        {<Trash />}
+                      </Button>
+                    </div>
+                  </li>
+                );
+              })
+            ) : (
+              <li className="text-lg text-center">Cart empy.</li>
+            )}
+          </ul>
+        </div>
+      </SheetContent>
+    </Sheet>
   );
 };
