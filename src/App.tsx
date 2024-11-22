3abf151337b1efdd2273227ac6146abaf20bdabf
kevoffdev <kevinsuviacruz@gmail.com>
2024-11-22 19:11:51 -0300

    reorganising the app

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,23 @@ import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { Cart } from "./components/Cart";
 import { CartProvider } from "./context/cart";
+import { Filters } from "./components/Filters";
 
 export function App() {
   const { products } = useFilter();
 
   return (
     <CartProvider>
-      <div className="mx-auto grid min-h-screen max-w-screen-lg grid-rows-[auto,auto,1fr,50px]">
-        <Header />
-        <Cart />
-        {products ? <Products /> : <div>loading...</div>}
-        <Footer />
+      <div className="bg-background text-primary">
+        <div className="m-auto grid min-h-screen px-10  md:max-w-screen-lg grid-rows-[auto,auto,1fr,50px]">
+          <Header />
+          <Cart />
+          <main className="my-4">
+            <Filters />
+            {products ? <Products /> : <div>loading...</div>}
+          </main>
+          <Footer />
+        </div>
       </div>
     </CartProvider>
   );
