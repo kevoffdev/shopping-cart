19297ddc6bff5ea86e2e18e7dc2d86b8d7dff3d6
kevoffdev <kevinsuviacruz@gmail.com>
2024-11-22 19:16:29 -0300

    separate components and replace them by shacd ui components

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,55 +1,73 @@
 import { OptionCategorie } from "../types";
 import { useFilter } from "../hooks/useFilter";
 
-export const Filters = () => {
-  const { filters, categories, setFilters } = useFilter();
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Slider } from "@/components/ui/slider";
 
-  const onChangeSelectedCategorie = (value: string | OptionCategorie) => {
-    setFilters({ ...filters, categorie: value });
-  };
+export const Filters = () => {
+  return (
+    <div className="flex flex-col justify-between gap-4 mb-4 sm:flex-row">
+      <SliderPrice />
+      <SelectCategories />
+    </div>
+  );
+};
+
+const SliderPrice = () => {
+  const { filters, setFilters } = useFilter();
+
   const onChangePrice = (value: number) => {
     setFilters({ ...filters, price: value });
   };
 
   return (
-    <div className="flex justify-between mb-3">
-      <div className="relative w-52">
-        <label htmlFor="labels-range-input">Precio: ${filters.price}</label>
-        <input
-          className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
-          id="labels-range-input"
+    <div className="relative">
+      <div>
+        <span className="font-semibold">Price: ${filters.price}</span>
+        <Slider
+          className="py-2"
           max={filters.maxPrice}
           min={filters.minPrice}
-          type="range"
-          value={filters.price}
-          onChange={(e) => onChangePrice(Number(e.target.value))}
+          step={1}
+          value={[filters.price]}
+          onValueChange={(e) => onChangePrice(e[0])}
         />
-        <div className="flex justify-between">
-          <span className="text-sm text-gray-500 dark:text-gray-400 start-0 -bottom-6">
-            Min (${filters.minPrice})
-          </span>
-          <span className="text-sm text-gray-500 dark:text-gray-400 end-0 -bottom-6">
-            Max (${filters.maxPrice})
-          </span>
-        </div>
       </div>
-      <div>
-        <p>Categories: </p>
-        <select
-          className="border border-white"
-          value={filters.categorie}
-          onChange={(e) => onChangeSelectedCategorie(e.target.value)}
-        >
-          <option value={OptionCategorie.All}>{OptionCategorie.All}</option>
-          {categories.map((categorie) => {
-            return (
-              <option key={categorie} value={categorie}>
-                {categorie}
-              </option>
-            );
-          })}
-        </select>
+      <div className="flex justify-between">
+        <span className="text-sm text-muted-foreground">Min (${filters.minPrice})</span>
+        <span className="text-sm text-muted-foreground">Max (${filters.maxPrice})</span>
       </div>
     </div>
+  );
+};
+
+const SelectCategories = () => {
+  const { filters, categories, setFilters } = useFilter();
+  const onChangeSelectedCategorie = (value: string | OptionCategorie) => {
+    setFilters({ ...filters, categorie: value });
+  };
+
+  return (
+    <Select onValueChange={(e) => onChangeSelectedCategorie(e)}>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="All Products" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value={OptionCategorie.All}>{OptionCategorie.All}</SelectItem>
+        {categories.map((categorie) => {
+          return (
+            <SelectItem key={categorie} value={categorie}>
+              {categorie}
+            </SelectItem>
+          );
+        })}
+      </SelectContent>
+    </Select>
   );
 };
