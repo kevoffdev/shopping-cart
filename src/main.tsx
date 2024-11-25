a1868396d27b8564ed961df3a9ddcd860f87471c
kevoffdev <kevinsuviacruz@gmail.com>
2024-11-25 19:59:54 -0300

    clean main

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,9 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App.tsx";
 import "./index.css";
-import { FiltersProvider } from "./context/filters.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <FiltersProvider>
-      <App />
-    </FiltersProvider>
+    <App />
   </React.StrictMode>,
 );
