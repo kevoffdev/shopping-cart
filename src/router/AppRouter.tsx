8417a178692864c0ece040acb0368481ac7da87c
kevoffdev <kevinsuviacruz@gmail.com>
2024-11-25 20:05:51 -0300

    router

---
+++ src/router/AppRouter.tsx
@@ -0,0 +1,17 @@
+import { Navigate, Route, Routes } from "react-router";
+
+import { Home } from "@/pages/Home";
+import Product from "@/pages/ProductDetail";
+
+const AppRouter = () => {
+  return (
+    <Routes>
+      <Route element={<Home />} path="/" />
+      <Route element={<Product />} path="/:id" />
+
+      <Route element={<Navigate to="/panel/admin" />} path="/*" />
+    </Routes>
+  );
+};
+
+export default AppRouter;
