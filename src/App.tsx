9482174658c48839134fd75471d25da6646643e9
kevoffdev <kevinsuviacruz@gmail.com>
2024-11-25 19:59:44 -0300

    structuring app for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,18 @@
-import { useFilter } from "./hooks/useFilter";
-import { Products } from "./components/Products";
-import { Header } from "./components/Header";
-import { Footer } from "./components/Footer";
-import { Cart } from "./components/Cart";
+import { BrowserRouter } from "react-router";
+
 import { CartProvider } from "./context/cart";
-import { Filters } from "./components/Filters";
+import { FiltersProvider } from "./context/filters";
+import AppRouter from "./router/AppRouter";
 
 export function App() {
-  const { products } = useFilter();
-
   return (
-    <CartProvider>
-      <div className="bg-background text-primary">
-        <div className="m-auto grid min-h-screen px-10  md:max-w-screen-lg grid-rows-[auto,auto,1fr,50px]">
-          <Header />
-          <Cart />
-          <main className="my-4">
-            <Filters />
-            {products ? <Products /> : <div>loading...</div>}
-          </main>
-          <Footer />
-        </div>
-      </div>
-    </CartProvider>
+    <BrowserRouter>
+      <FiltersProvider>
+        <CartProvider>
+          <AppRouter />
+        </CartProvider>
+      </FiltersProvider>
+    </BrowserRouter>
   );
 }
 
